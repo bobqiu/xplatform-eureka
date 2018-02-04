5de02f4f06824dcacf2c6c52fb5f11417f78ea01
bobqiu <19881xy@163.com>
2018-02-04 23:25:24 +0800

    init

---
+++ src/main/java/com/qiu/eureka/SpringCloudEurekaApplication.java
@@ -0,0 +1,19 @@
+package com.qiu.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Describe:
+ * Created by: bobqiu
+ * Date: 2018/2/4 上午12:27
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
+}
